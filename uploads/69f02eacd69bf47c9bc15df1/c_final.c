1963c7f5987841e8c06ac05154e922eada714502
joshuafrancis07 <joshuafrancisjf7@gmail.com>
2026-07-06 14:53:17 +0530

    initial commit

---
+++ uploads/69f02eacd69bf47c9bc15df1/c_final.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main ()
+{
+int p;
+printf("enter the pin :");
+int c =1;
+while(c<=3){
+scanf("%d",&p);
+
+if(p==1234)
+ {
+
+  printf("pin is correct ");
+  break;
+}
+else
+printf("pin is incorrect \n renter the pin:");
+  c++;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
